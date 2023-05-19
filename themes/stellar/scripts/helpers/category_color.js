2730e636a1e1ca64bc7bfd8ff22d8bc0f8f8cf6f
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-05-19 22:06:22 +0800

    up

--- themes/stellar/scripts/helpers/category_color.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-hexo.extend.helper.register('category_color', function(cat){
-  const cfg = hexo.theme.config;
-  if (cfg.article.category_color && cfg.article.category_color[cat]) {
-    return ' style="color:' + cfg.article.category_color[cat] + '"';
-  }
-  return '';
-});
--- themes/stellar/scripts/helpers/scrollreveal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-hexo.extend.helper.register('scrollreveal', function(args){
-  const cfg = hexo.theme.config;
-  if (cfg.plugins.scrollreveal && cfg.plugins.scrollreveal.enable) {
-    return ' reveal';
-  }
-  return '';
-});
